2c4d2d223c19be6f893db9e903165246ffe43a9c
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-01-26 06:36:12 +0000

    jsfingerprint

--- _templates/_head.php
+++ _templates/_head.php
@@ -5,6 +5,22 @@
     <meta name="author" content="Selfmade Ninja Academy">
     <meta name="generator" content="Hugo 0.88.1">
     <title>Login to Photogram</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+    // Initialize the agent at application startup.
+    const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
+        .then(FingerprintJS => FingerprintJS.load())
+
+    // Get the visitor identifier when you need it.
+    fpPromise
+        .then(fp => fp.get())
+        .then(result => {
+        // This is the visitor identifier:
+        const visitorId = result.visitorId
+        console.log(visitorId)
+        $('#fingerprint').val(visitorId);
+        })
+    </script>
 
     <!-- Bootstrap core CSS -->
     <link href="<?=get_config('base_path')?>assets/dist/css/bootstrap.min.css" rel="stylesheet">
