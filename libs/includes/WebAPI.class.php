977f07f2eedf6853dd7b34d706d60f86ec20873c
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-01-26 09:18:41 +0000

    webAPI

---
+++ libs/includes/WebAPI.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class WebAPI
+{
+    public function __construct()
+    {
+        global $__site_config;
+        if (php_sapi_name() == "cli") {
+            $__site_config_path = "/home/guruprasanth17102003/photogramconfig.json";
+            $__site_config = file_get_contents($__site_config_path);
+        } else {
+            $__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']).'/photogramconfig.json';
+            $__site_config = file_get_contents($__site_config_path);
+        }
+        Database::getConnection();
+    }
+
+    public function initiateSession()
+    {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            Session::start();
+        }
+    }
+}
--- libs/load.php
+++ libs/load.php
@@ -3,22 +3,10 @@ include_once 'includes/Session.class.php';
 include_once 'includes/User.class.php';
 include_once 'includes/Database.class.php';
 include_once 'includes/UserSession.class.php';
+include_once 'includes/WebAPI.class.php';
 
-global $__site_config;
-$__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : 
-            $_SERVER['DOCUMENT_ROOT']).'/photogramconfig.json';
-if (!file_exists($__site_config_path)) {
-    die("Configuration file not found: $__site_config_path");
-}
-$__site_config = file_get_contents($__site_config_path);
-
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    Session::start();
-}
-function load_template($name)
-{
-    include $_SERVER['DOCUMENT_ROOT'] . get_config('base_path'). "_templates/$name.php";
-}
+$wapi = new WebAPI();
+$wapi->initiateSession();
 
 function get_config($key, $default=null)
 {
@@ -31,4 +19,7 @@ function get_config($key, $default=null)
     }
 }
 
-
+function load_template($name)
+{
+    include $_SERVER['DOCUMENT_ROOT'] . get_config('base_path'). "_templates/$name.php";
+}
