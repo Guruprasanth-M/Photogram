7b9f28d54970dab556316d4506a45ec683b16dc9
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-02-14 16:52:23 +0000

    Update calltoaction.php with simplified form layout and structure

--- htdocs/_templates/index/calltoaction.php
+++ htdocs/_templates/index/calltoaction.php
@@ -1,17 +1,26 @@
-<section class="hero-section text-center container">
+<?php
+
+?>
+
+<section class="py-5 text-center container">
 	<div class="row py-lg-5">
-		<div class="col-lg-6 col-md-8 mx-auto">
-			<h1 class="hero-title">What are you up to, <span class="text-success"><?=Session::getUser()->getUsername()?></span>?</h1>
-			<p class="lead text-muted">Share a photo that talks about it.</p>
-			<form method="post" action="<?=get_config('base_path')?>sg.php" enctype="multipart/form-data">
-				<div class="mb-3">
-					<textarea id="post_text" name="post_text" class="form-control bg-dark text-light border-secondary" placeholder="What's on your mind?" rows="3"></textarea>
-				</div>
+		<form method="post" action="sg.php" enctype="multipart/form-data">
+			<div class="col-lg-6 col-md-8 mx-auto">
+				<h1 class="fw-light">What are you upto,
+					<?=Session::getUser()->getUsername()?>?
+				</h1>
+				<p class="lead text-muted">Share a photo that talks about it.</p>
+				<textarea id="post_text" name="post_text" class="form-control" placeholder="What are you upto?"
+					rows="3"></textarea>
 				<div class="input-group mb-3">
-					<input type="file" class="form-control bg-dark text-light border-secondary" name="post_image" id="post_image" accept="image/*">
+					<input type="file" accept="image/*" class="form-control" name="post_image" id="inputGroupFile02">
+					<!-- <label class="input-group-text" for="inputGroupFile02">Upload</label> -->
 				</div>
-				<button class="btn btn-success btn-lg px-5" type="submit">Share Memory</button>
-			</form>
-		</div>
+				<p>
+					<button class="btn btn-success my-2" type="submit">Share memory</button>
+					<!-- <a href="#" class="btn btn-secondary my-2">Clear</a> -->
+				</p>
+			</div>
+		</form>
 	</div>
 </section>
