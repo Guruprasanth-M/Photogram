10b07014ca72bbdb7ae786795d22e0ffe02fc85f
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-02-14 16:35:32 +0000

    image validation, retrival and rewrites

--- htdocs/libs/traits/SQLGetterSetter.trait.php
+++ htdocs/libs/traits/SQLGetterSetter.trait.php
@@ -8,7 +8,8 @@
  * $conn - The MySQL Connection.
  * $table - The MySQL Table Name.
  */
-trait SQLGetterSetter {
+trait SQLGetterSetter
+{
     public function __call($name, $arguments)
     {
         $property = preg_replace("/[^0-9a-zA-Z]/", "", substr($name, 3));
@@ -18,35 +19,43 @@ trait SQLGetterSetter {
         } elseif (substr($name, 0, 3) == "set") {
             return $this->_set_data($property, $arguments[0]);
         } else {
-            throw new Exception("__call() -> $name, function unavailable.");
+            throw new Exception(__CLASS__ . "::__call() -> $name, function unavailable.");
         }
     }
 
     private function _get_data($var)
     {
-        if (!$this->conn) {
-            $this->conn = Database::getConnection();
-        }
-        $sql = "SELECT `$var` FROM `$this->table` WHERE `id` = $this->id";
-        $result = $this->conn->query($sql);
-        if ($result and $result->num_rows == 1) {
-            return $result->fetch_assoc()["$var"];
-        } else {
-            return null;
+        try {
+            if (!$this->conn) {
+                $this->conn = Database::getConnection();
+            }
+            $sql = "SELECT `$var` FROM `$this->table` WHERE `id` = $this->id";
+            $result = $this->conn->query($sql);
+            if ($result and $result->num_rows == 1) {
+                return $result->fetch_assoc()["$var"];
+            } else {
+                return null;
+            }
+        } catch (Exception $e) {
+            throw new Exception(__CLASS__ . "::_get_data() -> $var, data unavailable.");
         }
     }
 
     private function _set_data($var, $data)
     {
-        if (!$this->conn) {
-            $this->conn = Database::getConnection();
-        }
-        $safe = $this->conn->real_escape_string($data);
-        $sql = "UPDATE `$this->table` SET `$var`='$safe' WHERE `id`=$this->id;";
-        if ($this->conn->query($sql)) {
-            return true;
-        } else {
-            return false;
+        try {
+            if (!$this->conn) {
+                $this->conn = Database::getConnection();
+            }
+            $safe = $this->conn->real_escape_string($data);
+            $sql = "UPDATE `$this->table` SET `$var`='$safe' WHERE `id`=$this->id;";
+            if ($this->conn->query($sql)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception(__CLASS__ . "::_set_data() -> $var, data unavailable.");
         }
     }
 }
